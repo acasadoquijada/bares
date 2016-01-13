154709fb13a0f889da6a275cc558118d9c599e71
alejandro <acasadoquijada@gmail.com>
2016-01-13 17:50:39 +0100

    Añadido test

--- bares/tests.py
+++ bares/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+from .models import Bar,Tapa
+
 # Create your tests here.
+
+class TestBarTestCase):
+
+	def test_crear_bar(self):
+		b = Bar(nombre="bar", direccion="direccion",numero_visita=0)
+		b.save()
+		self.assertEqual(b.nombre, "bar")
+		print("Test: creación y salvado de bar correcto")
+
