ea6b040c31250a6d4c6a1241fb0d28f08edc5c26
alejandro <acasadoquijada@gmail.com>
2016-01-14 21:30:03 +0100

    Error

--- practica4/settings.py
+++ practica4/settings.py
@@ -138,6 +138,9 @@ STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 REGISTRATION_OPEN = True                # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
